62878ce53a11830b9e701e4620ed3512bb24aa7c
wellbigas <77459233+wellbigas@users.noreply.github.com>
2021-06-18 20:22:49 -0300

    Initial commit

---
+++ src/TestaPontoFlutuante.java
@@ -0,0 +1,18 @@
+public class TestaPontoFlutuante {
+
+    public static void main(String[] args){
+
+        double salario = 1250.70;
+        System.out.println("meu salário é " + salario);
+
+        double divisao = 3.14 / 2;
+        System.out.println(divisao);
+
+        double outraDivisao = 5 / 2; //arredonda//
+        System.out.println(outraDivisao);
+
+        double novaTentativa = 5.0 / 2;
+        System.out.println(novaTentativa);
+
+    }
+}
